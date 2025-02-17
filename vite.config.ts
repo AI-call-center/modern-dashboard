7b70131d4b052783cd8b0bc1cbf241f2b38d90f0
gitdevin99 <sanjananb1@gmail.com>
2025-02-17 14:50:35 +0400

    fix: skip typescript checks during build for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
   plugins: [react()],
   server: {
     port: 5173,
